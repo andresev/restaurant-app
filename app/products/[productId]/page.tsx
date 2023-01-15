f87636cc5684ee670d341775062c64f067b81019
andresev <andresevaldez@gmail.com>
2023-01-14 23:42:12 -0600

    product page styling

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -33,64 +33,50 @@ const ProductPage = async ({ params: { productId } }: PageProps) => {
   const product = await getProduct(productId);
   console.log(product);
   return (
-    <div className="flex-1 bg-[#f8dcc9]">
-      <div className="">
-        <Image className="w-36 h-36 rounded-full" alt="wing-combo" src={img} />
-        <div className="">
-          <h3 className="text-black">{product.title}</h3>
-          <div>
-            <span className="">{product.prices[0]}</span>
+    <div className="flex-1 h-screen w-screen bg-[#f8dcc9]">
+      <Image className="w-36 h-36 rounded-full" alt="wing-combo" src={img} />
+      <h3 className="text-black">{product.title}</h3>
+      <span className="">{product.prices[0]}</span>
+      <p className="w-[60%] text-lg text-black">{product.description}</p>
+      <div>
+        <h3 className="text-2xl font-bold text-orange-500">
+          Choose your flavor ({`${product.flavors[0].limit}`})
+        </h3>
+        {product.flavors.map((items: any, index: number) => (
+          <div className="" key={index}>
+            {items.text.map((flavor: string, index: number) => (
+              <div key={index} className="">
+                <input
+                  className="w-4 h-4 shadow rounded-sm"
+                  type="checkbox"
+                  id={flavor}
+                  name={flavor}
+                />
+                <span key={index} className="text-lg text-black pl-2">
+                  {flavor}
+                </span>
+              </div>
+            ))}
           </div>
-          <p className="w-[60%] text-lg text-black">{product.description}</p>
-          <div>
-            <h3 className="text-2xl font-bold text-orange-500">
-              Choose your flavor ({`${product.flavors[0].limit}`})
-            </h3>
-            <div className="flex flex-row justify-between w-[60%] ">
-              {product.flavors.map((items: any, index: number) => (
-                <div className="flex flex-col justify-between" key={index}>
-                  {items.text.map((flavor: string, index: number) => (
-                    <div key={index} className="flex flex-row items-center">
-                      <input
-                        className="w-6 h-6 shadow rounded-md"
-                        type="checkbox"
-                        id={flavor}
-                        name={flavor}
-                      />
-                      <span key={index} className="text-lg text-black pl-2">
-                        {flavor}
-                      </span>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
+        ))}
+      </div>
+      <div>
+        <h3 className="text-xl font-bold text-orange-500">Add-ons</h3>
+        {product.extraOptions.map((option: any, index: number) => (
+          <div key={index} className="flex flex-row">
+            <input
+              className="w-4 h-4 shadow rounded-sm"
+              type="checkbox"
+              id={option.text}
+              name={option.text}
+              // onChange={(e) => handleChange(e, option)}
+            />
+            <label className="pr-2 text-lg text-black">{option.text}</label>
+            <label className="text-lg text-black">
+              {`($${option.price.toFixed(2)})`}
+            </label>
           </div>
-          <div>
-            <h3 className="text-xl font-bold text-orange-500">Add-ons</h3>
-            <div className="flex flex-row">
-              {product.extraOptions.map((option: any, index: number) => (
-                <div key={index} className="flex flex-row items-center">
-                  <input
-                    className="w-6 h-6 shadow rounded-md"
-                    type="checkbox"
-                    id={option.text}
-                    name={option.text}
-                    // onChange={(e) => handleChange(e, option)}
-                  />
-                  <div className="pl-3">
-                    <label className="pr-2 text-lg text-black">
-                      {option.text}
-                    </label>
-                    <label className="text-lg text-black">
-                      {`($${option.price.toFixed(2)})`}
-                    </label>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
